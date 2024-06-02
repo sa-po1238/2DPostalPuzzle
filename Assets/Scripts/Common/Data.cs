68f18ec0d3aaaa1f77185990309250c10dcfc0e9
Saya Kikuchi <60276072.kikuchi.cgp@gmail.com>
2024-06-02 23:19:04 +0900

    [add] データ管理

---
+++ Assets/Scripts/Common/Data.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // データの初期化
+    private void SetData()
+    {
+        PlayerPrefs.SetInt("Day", 1);
+        PlayerPrefs.SetInt("Score", 0);
+        PlayerPrefs.SetInt("Miss", 0);
+        PlayerPrefs.SetInt("Money", 0);
+    }
+
+    // 日にちのセット
+    public void SetDay(int day)
+    {
+        PlayerPrefs.SetInt("Day", day);
+    }
+
+    // 日にちの取得
+    public int GetDay()
+    {
+        return PlayerPrefs.GetInt("Day");
+    }
+
+    // スコアのセット
+    public void SetScore(int score)
+    {
+        PlayerPrefs.SetInt("Score", score);
+    }
+
+    // スコアの取得
+    public int GetScore()
+    {
+        return PlayerPrefs.GetInt("Score");
+    }
+
+    // ミスのセット
+    public void SetMiss(int miss)
+    {
+        PlayerPrefs.SetInt("Miss", miss);
+    }
+
+    // ミスの取得
+    public int GetMiss()
+    {
+        return PlayerPrefs.GetInt("Miss");
+    }
+
+    // 残金のセット
+    public void SetMoney(int money)
+    {
+        PlayerPrefs.GetInt("Money", money);
+        money +=
+        PlayerPrefs.SetInt("Money", money);
+    }
+
+    // 残金の取得
+    public int GetMoney()
+    {
+        return PlayerPrefs.GetInt("Money");
+    }
+}
